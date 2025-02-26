13eedfa70e1efd2110630f407c653a22c9b95d0b
Devaditya borah <72392682+Devaditya-Suuu@users.noreply.github.com>
2025-02-26 17:10:47 +0530

    Delete java directory

--- java/exam_testing.c
+++
@@ -1,9 +0,0 @@
-#include<stdlib.h>
-#include<stdio.h>
-int main(){
-    int value = 9876;
-    printf("%d \n", value);
-    printf("%6d \n", value);
-    printf("%2d \n", value);
-    printf("%06d u\n", value);
-}
